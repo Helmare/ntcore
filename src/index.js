adafa3a480c4b4f7cd46f8e4ef5af1638a94367e
Hazdryx <hazdryx@gmail.com>
2022-05-21 08:35:06 -0500

    set pitch and speed defaults to match eachother

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@
 const chalk = require('chalk');
 const { Command } = require('commander');
 const { argv } = require('process');
-const { getAudioInfo, nightcore } = require('./ffmpeg');
+const { nightcore } = require('./ffmpeg');
 
 const program = new Command()
   .argument('<path>', 'path of output audio')
-  .option('-p, --pitch <pitch>', 'pitch of the output as a decimal', '1')
-  .option('-s, --speed <speed>', 'speed of the output as a decimal', '1')
+  .option('-p, --pitch <pitch>', 'pitch of the output as a decimal')
+  .option('-s, --speed <speed>', 'speed of the output as a decimal')
   .requiredOption('-o, --output <path>', 'output path')
 
 program.action(async (args, options) => {
+  if (options.pitch && !options.speed) {
+    options.speed = options.pitch;
+  }
+  else if (options.speed && !options.pitch) {
+    options.pitch = options.speed
+  }
+  else {
+    options.pitch = '1';
+    options.speed = '1';
+  }
+
   await nightcore(program.args[0], options.pitch, options.speed, options.output);
 });
 
